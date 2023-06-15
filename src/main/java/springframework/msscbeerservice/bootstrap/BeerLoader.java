2e956934c81a7cc5506002d0e05f565728b1d7c4
Heyward Hagenbuch <Heyward360@gmail.com>
2023-06-15 10:16:42 -0400

    added a beer loader to store initial beer data

---
+++ src/main/java/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,50 @@
+package springframework.msscbeerservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import springframework.msscbeerservice.domain.Beer;
+import springframework.msscbeerservice.repositories.BeerRepository;
+
+import java.math.BigDecimal;
+
+//@RequiredArgsConstructor
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    //CommandLine Runner will run every time the spring context is started
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Blue Moon")
+                    .beerStyle("Pale Ale")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("Pale Ale")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        System.out.println("Loaded Beers: " + beerRepository.count());
+    }
+}
--- src/main/java/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,2 +1,10 @@
-package springframework.msscbeerservice.repositories;public interface BeerRepository {
+package springframework.msscbeerservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springframework.msscbeerservice.domain.Beer;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
 }
